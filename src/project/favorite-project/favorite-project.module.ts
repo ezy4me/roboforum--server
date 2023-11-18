443b4d28d4099755708ff89e054d4fe9d407b9a8
ezy4me <mrronal@mail.ru>
2023-11-19 00:45:21 +0300

    added user get post delete favorite project(need to test)

--- src/project/favorite-project/favorite-project.module.ts
+++ src/project/favorite-project/favorite-project.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FavoriteProjectService } from './favorite-project.service';
 import { FavoriteProjectController } from './favorite-project.controller';
+import { DatabaseService } from '@database/database.service';
 
 @Module({
-  providers: [FavoriteProjectService],
-  controllers: [FavoriteProjectController]
+  providers: [FavoriteProjectService, DatabaseService],
+  controllers: [FavoriteProjectController],
+  exports: [FavoriteProjectService],
 })
 export class FavoriteProjectModule {}
--- src/project/favorite-project/favorite-project.service.ts
+++ src/project/favorite-project/favorite-project.service.ts
@@ -1,4 +1,48 @@
+import { DatabaseService } from '@database/database.service';
 import { Injectable } from '@nestjs/common';
+import { FavoriteProject } from '@prisma/client';
 
 @Injectable()
-export class FavoriteProjectService {}
+export class FavoriteProjectService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async getUserFavoriteProjects(userId: number): Promise<FavoriteProject[]> {
+    return this.databaseService.favoriteProject.findMany({
+      where: { userId },
+    });
+  }
+
+  async createUserFavoriteProjects(
+    userId: number,
+    projectId: number,
+  ): Promise<FavoriteProject> {
+    return this.databaseService.favoriteProject.create({
+      data: {
+        userId: userId,
+        projectId: projectId,
+      },
+    });
+  }
+
+  async deleteUserFavoriteProject(
+    userId: number,
+    projectId: number,
+  ): Promise<FavoriteProject> {
+    const userFavoriteProject =
+      await this.databaseService.favoriteProject.findMany({
+        where: {
+          userId,
+        },
+      });
+
+    const uniqueProject = await userFavoriteProject.filter(
+      (i) => i.projectId == projectId,
+    );
+
+    return this.databaseService.favoriteProject.delete({
+      where: {
+        id: uniqueProject[0].id,
+      },
+    });
+  }
+}
