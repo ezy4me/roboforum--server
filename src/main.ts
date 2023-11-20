38bbd216d662b19a3c6c5f7569d5dfee4624aae4
ezy4me <mrronal@mail.ru>
2023-11-20 22:39:25 +0300

    deploy version v1

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
   const corsOptions: CorsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://roboforum-client.onrender.com'],
     credentials: true,
   };
 
