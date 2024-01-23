87def6f34d7b1de3c2abf66f3316e26f9e1ca514
ezy4me <mrronal@mail.ru>
2024-01-24 00:17:34 +0300

    final

--- src/discussion/favorite-discussion/favorite-discussion.module.ts
+++ src/discussion/favorite-discussion/favorite-discussion.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FavoriteDiscussionService } from './favorite-discussion.service';
 import { FavoriteDiscussionController } from './favorite-discussion.controller';
+import { DatabaseService } from '@database/database.service';
 
 @Module({
-  providers: [FavoriteDiscussionService],
-  controllers: [FavoriteDiscussionController]
+  providers: [FavoriteDiscussionService, DatabaseService],
+  controllers: [FavoriteDiscussionController],
 })
 export class FavoriteDiscussionModule {}
